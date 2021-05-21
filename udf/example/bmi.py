2496e895be86c0a64688f0825ac1993fca016922
Zhe Wu <zwu@kinetica.com>
2021-05-21 15:39:27 -0400

    mod bmi.py

--- udf/example/bmi.py
+++ udf/example/bmi.py
@@ -6,6 +6,7 @@ df = proc_data.to_df()
 df["BMI"] = df["weightKg"]/df["heightM"]
 
 output_table = proc_data.output_data[0]
-
 output_table.size = df.shape[0]
 proc_data.from_df(df, output_table)
+
+proc_data.complete()
